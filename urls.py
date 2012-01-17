371e5d8b1e4bec963bd4fd56b639fcea72fc4847
quek <quekshuy@gmail.com>
2012-01-17 14:39:11 +0800

    Finished landing page layout with scroller

--- urls.py
+++ urls.py
@@ -1,17 +1,25 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^around_landing/', include('around_landing.foo.urls')),
+
+    (r'^$', TemplateView.as_view(template_name='pages/start_page.html')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+   (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+   (r'^admin/', include(admin.site.urls)),
 )
+
+from django.conf import settings
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
