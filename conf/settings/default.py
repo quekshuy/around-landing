c0b7fd44e8047e0a10d3c323f901ba4e66cb9ac3
quek <quekshuy@gmail.com>
2012-01-25 15:10:19 +0800

    Modified to add staticfiles app to INSTALLED_APPS

--- conf/settings/default.py
+++ conf/settings/default.py
@@ -78,7 +78,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'around_landing.urls'
+#ROOT_URLCONF = 'around_landing.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -92,6 +93,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'south',
     'leads',
